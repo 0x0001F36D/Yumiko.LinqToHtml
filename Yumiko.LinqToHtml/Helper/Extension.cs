eed89a8a579f0f47fc09b1e53a75a9e804756cbb
0x0001F36D <hanabiranoshitaniumore@gmail.com>
2017-03-22 00:03:27 +0800

    Fixed Accessor And Removed Unused Namespaces

--- Yumiko.LinqToHtml/Helper/Extension.cs
+++ Yumiko.LinqToHtml/Helper/Extension.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Yumiko.LinqToHtml.Interfaces;
-using Yumiko.LinqToHtml.Tags.Infrastructure;
-
+﻿
 namespace Yumiko.LinqToHtml.Helper
 {
-    public static partial class Extension
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+    using Tags.Infrastructure;
+
+    internal static partial class Extension
     {
-        static readonly Regex split_rule = new Regex("(<[^>]+>)");
-        public static IEnumerable<string> HtmlSeparator(string html) => split_rule.Split(html);
-        public static string OmitEmptyCharacter(this string source) => source.Replace(Tag.EmptyCharacter.ToString(), null);
+        private static readonly Regex split_rule = new Regex("(<[^>]+>)");
+
+        internal static IEnumerable<string> HtmlSeparator(string html) => split_rule.Split(html);
+
+        internal static string OmitEmptyCharacter(this string source) => source.Replace(Tag.EmptyCharacter.ToString(), null);
     }
 }
